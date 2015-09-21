aae31ab4e5f7dc808507d1b9fecc67e9ceb64708
Namoor <jannikso@web.de>
2015-09-21 15:00:16 +0200

    Pause 21_09

--- GraphicsSystemProgramming/Main.cpp
+++ GraphicsSystemProgramming/Main.cpp
@@ -1,13 +1,20 @@
 #include <Windows.h>
 
-bool isRunning;
+#include <D3D11.h>
+#include <D3DX11.h>
+#include <D3DX10.h>
+
+
+// WM_QUIT: Application soll beendet werden
+// WM_DESTROY: Window wurde destroyed
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
 {
-	if (Message == WM_QUIT)
+	if (Message == WM_DESTROY)
 	{
 		//Programm beenden
-		isRunning = false;
+
+		PostQuitMessage(0);
 	}
 
 	return DefWindowProc(hwnd,Message,wParam,lParam);
@@ -58,16 +65,75 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	MSG msg;
 
-	isRunning = true;
+	ID3D11Device* m_pDevice;
+	ID3D11DeviceContext* m_pDeviceContext;
+
+	IDXGISwapChain* m_pSwapChain;
+
+	D3D_FEATURE_LEVEL _FeatureLevel = D3D_FEATURE_LEVEL::D3D_FEATURE_LEVEL_11_0;
+
+	D3D_FEATURE_LEVEL _FeatureLevelOut;
+
+	DXGI_SWAP_CHAIN_DESC _Desc;
+	ZeroMemory(&_Desc, sizeof(_Desc));
+
+	_Desc.BufferCount = 1;
+	_Desc.BufferDesc.Format = DXGI_FORMAT::DXGI_FORMAT_R8G8B8A8_UNORM;
+	_Desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	_Desc.OutputWindow = _Window;
+	_Desc.Windowed = true;
+	_Desc.SampleDesc.Count = 4;
+
+
+	HRESULT _HR = D3D11CreateDeviceAndSwapChain(nullptr,
+		D3D_DRIVER_TYPE::D3D_DRIVER_TYPE_HARDWARE,
+		nullptr,
+		0, // D3D11_CREATE_DEVICE_FLAG::D3D11_CREATE_DEVICE_DEBUG,
+		&_FeatureLevel,
+		1,
+		D3D11_SDK_VERSION,
+		&_Desc,
+		&m_pSwapChain,
+		&m_pDevice,
+		&_FeatureLevelOut,
+		&m_pDeviceContext);
+	
+
+	// ID3D11Texture2D : Textur
+
+	// ID3D11ShaderResourceView : Objekt um aus der Textur zu lesen
+	// ID3D11RenderTargetView : Objekt um in die Textur zu schreiben
+
+	ID3D11RenderTargetView* m_pBackBuffer;
+
+	ID3D11Texture2D* m_pBackbufferTexture = nullptr;
+	m_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&m_pBackbufferTexture);
+	
+	m_pDevice->CreateRenderTargetView(m_pBackbufferTexture, nullptr, &m_pBackBuffer);
 
 	//GameLoop
-	while (isRunning)
+	while (true)
 	{
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 		{
 			//Message bearbeiten
 			TranslateMessage(&msg);
-			DispatchMessage(&msg);			
+			DispatchMessage(&msg);
+
+			if (msg.message == WM_QUIT)
+				break;
 		}
+
+
+		float _Color[4];
+		_Color[0] = (rand() % 256) / 255.0f;
+		_Color[1] = (rand() % 256) / 255.0f;
+		_Color[2] = (rand() % 256) / 255.0f;
+		_Color[3] = 1;
+
+		m_pDeviceContext->ClearRenderTargetView(m_pBackBuffer, _Color);
+
+		m_pSwapChain->Present(0, 0);
+
 	}
 }
